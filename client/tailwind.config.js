ec5c772c90b325b7a8d38499c8f3feee49fab4eb
Bidyut kundu <106534797+bidyut10@users.noreply.github.com>
2024-12-20 01:44:53 +0530

    Css fixes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+  extend: {},
 };
+export const plugins = [];
+export const autoprefixer = {};
